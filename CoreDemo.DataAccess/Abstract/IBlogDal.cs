5273736ec86f3dbb89a87b8a01007a7b3e3c2230
Erkan <erkan.salihoglu@hotmail.com>
2022-12-21 09:14:39 +0300

    14.ders yapıldı
    Dal işlemleri çalışmaları

---
+++ CoreDemo.DataAccess/Abstract/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using CoreDemo.Project.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace CoreDemo.Project.DataAccess.Abstract
+{
+    public interface IBlogDal
+    {
+        List<Blog> GetBlogs();
+
+        Blog GetBlog(int id);
+
+        void AddBlog(Blog blog);
+
+        void UpdateBlog(Blog blog);
+
+        void DeleteBLog(Blog blog);
+    }
+}
---
+++ CoreDemo.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using CoreDemo.Project.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace CoreDemo.Project.DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+
+        List<Category> GetCategories();
+
+        Category GetCategory(int id);
+
+        void AddCategory(Category category);
+
+        void UpdateCategory(Category category);
+
+        void DeleteCategory(Category category);
+        
+
+    }
+}
